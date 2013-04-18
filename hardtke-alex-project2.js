30df507918b27012af7dee4b3e9467d1f0804f9d
Alex Hardtke <Alex@Alexs-MacBook-Pro.local>
2013-04-18 14:36:11 -0500

    Update 2:36pm

--- hardtke-alex-project2.js
+++ hardtke-alex-project2.js
@@ -1,1 +1,28 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+
+// Alex Hardtke
+// SDI 1304
+// Project 2
+
+// Global Variables
+
+var lunchTime = "It's lunchtime!",
+	numSandwiches = 2,
+	bagTypes = ["paper bag", "plastic bag"]
+
+// Procedure
+
+var myMeal = function(wantSandwiches, sandwichesPacked){
+	
+	var totalSandwiches =  wantSandwiches - sandwichesPacked;
+	if (totalSandwiches <= 1){
+		console.log(lunchTime + " I had a big breakfast, so I'm not too hungry. Good thing I just packed " + totalSandwiches + " sandwich.");
+	} else
+	console.log(lunchTime + " I'm starving! Good think I packed " + totalSandwiches + " sandwiches!");
+	
+};
+
+myMeal(3,1);
+
+// Boolean Function
+
