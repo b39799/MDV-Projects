549a3601ef12a3df5363421e04333cf94670f7d4
Alex Hardtke <Alex@Alexs-MacBook-Pro.local>
2013-04-18 15:42:46 -0500

    Update 3:40pm

--- hardtke-alex-project2.js
+++ hardtke-alex-project2.js
@@ -18,11 +18,29 @@ var myMeal = function(wantSandwiches, sandwichesPacked){
 	if (totalSandwiches <= 1){
 		console.log(lunchTime + " I had a big breakfast, so I'm not too hungry. Good thing I just packed " + totalSandwiches + " sandwich.");
 	} else
-	console.log(lunchTime + " I'm starving! Good think I packed " + totalSandwiches + " sandwiches!");
+	console.log(lunchTime + " I'm starving! Good thing I packed " + totalSandwiches + " sandwiches!");
 	
 };
 
 myMeal(3,1);
 
 // Boolean Function
+
+var getPeanutButterJelly = function(hasPeanutButter, hasJelly){
+	
+	var ingredients = hasPeanutButter * hasJelly;
+	if (ingredients >= 1){
+		return true;
+	} else {
+		return false;
+	}
+	
+};
+
+var yesIngredients = getPeanutButterJelly(2,1);
+if (yesIngredients === true){
+console.log("It is " + yesIngredients + " that I can make a peanut butter and jelly sandwich. That sounds good!");
+} else {
+	console.log("It is " + yesIngredients + " that I can make a peanut butter and jelly sandwich. I need to find something else to make!");
+};
 
