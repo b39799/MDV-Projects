72111eb5fb8147aa4ad6ba43db2af16847585b97
Alex Hardtke <Alex@Alexs-MacBook-Pro.local>
2013-04-25 02:56:52 -0500

    Added JSON

--- hardtke-alex-project3.js
+++ hardtke-alex-project3.js
@@ -1,1 +1,37 @@
 //alert("JavaScript works!");
+
+// Alex Hardtke
+// 4-24-13
+// SDI 1304
+// Project 3
+// Objects!
+
+// Start
+
+// JSON
+
+var playerInfo = {
+
+	"players": [
+		{
+			"gamerTag": "Miller",
+			"rank":     "General",
+			"level":     50
+		},
+		{
+			"gamerTag": "Hudson",
+			"rank":     "Captain",
+			"level":     35
+		},
+		{
+			"gamerTag": "Smith",
+			"rank":     "Brigadier",
+			"level":     45
+		}
+	]
+};
+
+// Global Variables
+
+var gameTitle = "Halo 3",
+	
