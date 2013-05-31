a457932c1e38230a7ef0ea87d0bb5c9b5e9fd662
Alex Hardtke <Alex@Alexs-MacBook-Pro.local>
2013-05-30 20:01:09 -0500

    Added json

---
+++ js/json.js
@@ -0,0 +1,35 @@
+var json = {
+	"account1": {
+		"account": ["Account:", "Google"],
+		"email": ["Email:", "alex@gmail.com"],
+		"user": ["Username:", "AlexH"],
+		"password": ["Password:", "alex123"],
+		"confirm": ["Confirm:", "alex123"],
+		"primary": ["Primary Account?:", "Yes"],
+		"date": ["Date:", "2013-05-01"],
+		"range": ["Rate How Much You Use Account:", "8"],
+		"notes": ["Notes", "Google Plus account"]
+	},
+	"account2": {
+		"account": ["Account:", "School"],
+		"email": ["Email:", "alex@fullsail.edu"],
+		"user": ["Username:", "Alex123"],
+		"password": ["Password:", "alexh"],
+		"confirm": ["Confirm:", "alexh"],
+		"primary": ["Primary Account?:", "Yes"],
+		"date": ["Date:", "2012-10-01"],
+		"range": ["Rate How Much You Use Account:", "10"],
+		"notes": ["Notes", "Full Sail student account"]
+	},
+	"account3": {
+		"account": ["Account:", "Yahoo"],
+		"email": ["Email:", "alex@yahoo.com"],
+		"user": ["Username:", "AlexH123"],
+		"password": ["Password:", "alex612"],
+		"confirm": ["Confirm:", "alex612"],
+		"primary": ["Primary Account?:", "No"],
+		"date": ["Date:", "2006-06-01"],
+		"range": ["Rate How Much You Use Account:", "4"],
+		"notes": ["Notes", "Yahoo mail account"]
+	}
+}
