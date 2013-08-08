95946971a677677049a01cd7b1600c5f24b63296
Alex Hardtke <Alex@Alexs-MacBook-Pro.local>
2013-08-08 17:17:36 -0500

    Added week 1 files

---
+++ ASD_Project/js/main.js
@@ -0,0 +1,9 @@
+// Alex Hardtke, ASD 1308, Week 1
+
+$('#home').on('pageinit', function(){
+	//code for home page here
+});
+
+$('#additem').on('pageinit', function(){
+	//code for add item page here
+});
