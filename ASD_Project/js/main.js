cbc8c6aaa018f1a62055af558f3fffbfc142ca25
Alex Hardtke <Alex@Alexs-MacBook-Pro.local>
2013-08-12 19:27:18 -0500

    Added DOM Ready function

--- ASD_Project/js/main.js
+++ ASD_Project/js/main.js
@@ -1,4 +1,7 @@
-// Alex Hardtke, ASD 1308, Week 1
+// Alex Hardtke, ASD 1308, Week 2
+
+//DOM Ready
+$(function(){
 
 $('#home').on('pageinit', function(){
 	//code for home page here
@@ -7,3 +10,5 @@ $('#home').on('pageinit', function(){
 $('#additem').on('pageinit', function(){
 	//code for add item page here
 });
+
+}); // End DOM Ready
