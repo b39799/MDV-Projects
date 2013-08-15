0b71361a3d3de70fa022cba018b7720e382ff720
Alex Hardtke <Alex@Alexs-MacBook-Pro.local>
2013-08-15 17:14:25 -0500

    Added everything

--- ASD_Project/js/json.js
+++ ASD_Project/js/json.js
@@ -5,183 +5,73 @@
 
 var json = {
 	"account1": {
-		"account": ["Account:", "Google"],
-		"email": ["Email:", "alex@gmail.com"],
-		"user": ["Username:", "AlexH"],
-		"password": ["Password:", "alex123"],
-		"primary": ["Primary Account?:", "Yes"],
-		"type": ["Account Type:", "Personal"],
-		"notes": ["Notes:", "Google Plus account"]
+		"account": "Google",
+		"email": "alex@gmail.com",
+		"user": "AlexH",
+		"password": "alex123",
+		"notes": "Google Plus account"
 	},
 	"account2": {
-		"account": ["Account:", "Full Sail"],
-		"email": ["Email:", "alex@fullsail.edu"],
-		"user": ["Username:", "Alex123"],
-		"password": ["Password:", "alexh"],
-		"primary": ["Primary Account?:", "Yes"],
-		"type": ["Account Type:", "School"],
-		"notes": ["Notes:", "Full Sail student account"]
+		"account": "Full Sail",
+		"email": "alex@fullsail.edu",
+		"user": "Alex123",
+		"password": "alexh",
+		"notes": "Full Sail student account"
 	},
 	"account3": {
-		"account": ["Account:", "Amazon"],
-		"email": ["Email:", "alex@gmail.com"],
-		"user": ["Username:", "Alex123"],
-		"password": ["Password:", "alexhardtke"],
-		"primary": ["Primary Account?:", "Yes"],
-		"type": ["Account Type:", "Other"],
-		"notes": ["Notes:", "Amazon seller account"]
+		"account": "Amazon",
+		"email": "alex@gmail.com",
+		"user": "Alex123",
+		"password": "alexhardtke",
+		"notes": "Amazon seller account"
 	},
 	"account4": {
-		"account": ["Account:", "ESPN"],
-		"email": ["Email:", "alex@gmail.com"],
-		"user": ["Username:", "AlexHardtke"],
-		"password": ["Password:", "alexh1"],
-		"primary": ["Primary Account?:", "Yes"],
-		"type": ["Account Type:", "Personal"],
-		"notes": ["Notes:", "ESPN account login"]
+		"account": "ESPN",
+		"email": "alex@gmail.com",
+		"user": "AlexHardtke",
+		"password": "alexh1",
+		"notes": "ESPN account login"
 	},
 	"account5": {
-		"account": ["Account:", "Facebook"],
-		"email": ["Email:", "alex@gmail.com"],
-		"user": ["Username:", "Alex Hardtke"],
-		"password": ["Password:", "alexh"],
-		"primary": ["Primary Account?:", "Yes"],
-		"type": ["Account Type:", "Personal"],
-		"notes": ["Notes:", "Facebook account login"]
+		"account": "Facebook",
+		"email": "alex@gmail.com",
+		"user": "Alex Hardtke",
+		"password": "alexh",
+		"notes": "Facebook account login"
 	},
 	"account6": {
-		"account": ["Account:", "iTunes"],
-		"email": ["Email:", "alex@gmail.com"],
-		"user": ["Username:", "alex@gmail.com"],
-		"password": ["Password:", "alexhardtke5"],
-		"primary": ["Primary Account?:", "Yes"],
-		"type": ["Account Type:", "Entertainment"],
-		"notes": ["Notes:", "My iTunes account"]
+		"account": "iTunes",
+		"email": "alex@gmail.com",
+		"user": "alex@gmail.com",
+		"password": "alexhardtke5",
+		"notes": "My iTunes account"
 	},
 	"account7": {
-		"account": ["Account:", "iTunes Alternate"],
-		"email": ["Email:", "ryan@yahoo.com"],
-		"user": ["Username:", "ryan123"],
-		"password": ["Password:", "ryanh"],
-		"primary": ["Primary Account?:", "No"],
-		"type": ["Account Type:", "Entertainment"],
-		"notes": ["Notes:", "Ryan's iTunes account"]
+		"account": "iTunes Alternate",
+		"email": "ryan@yahoo.com",
+		"user": "ryan123",
+		"password": "ryanh",
+		"notes": "Ryan's iTunes account"
 	},
 	"account8": {
-		"account": ["Account:", "YouTube"],
-		"email": ["Email:", "alex@gmail.com"],
-		"user": ["Username:", "AlexH"],
-		"password": ["Password:", "alex123"],
-		"primary": ["Primary Account?:", "Yes"],
-		"type": ["Account Type:", "Entertainment"],
-		"notes": ["Notes:", "Main YouTube account"]
+		"account": "YouTube",
+		"email": "alex@gmail.com",
+		"user": "AlexH",
+		"password": "alex123",
+		"notes": "Main YouTube account"
 	},
 	"account9": {
-		"account": ["Account:", "YouTube Band"],
-		"email": ["Email:", "alex@gmail.com"],
-		"user": ["Username:", "LLE"],
-		"password": ["Password:", "lle123"],
-		"primary": ["Primary Account?:", "No"],
-		"type": ["Account Type:", "Personal"],
-		"notes": ["Notes:", "My band's YouTube account"]
+		"account": "YouTube Band",
+		"email": "alex@gmail.com",
+		"user": "LLE",
+		"password": "lle123",
+		"notes": "My band's YouTube account"
 	},
 	"account10": {
-		"account": ["Account:", "Yahoo"],
-		"email": ["Email:", "alex@yahoo.com"],
-		"user": ["Username:", "alex@yahoo.com"],
-		"password": ["Password:", "alex123"],
-		"primary": ["Primary Account?:", "Yes"],
-		"type": ["Account Type:", "Personal"],
-		"notes": ["Notes:", "My Yahoo email account"]
-	},
-	"account11": {
-		"account": ["Account:", "Dominos"],
-		"email": ["Email:", "alex@gmail.com"],
-		"user": ["Username:", "AlexHardtke"],
-		"password": ["Password:", "alexh1"],
-		"primary": ["Primary Account?:", "Yes"],
-		"type": ["Account Type:", "Other"],
-		"notes": ["Notes:", "Dominos pizza account"]
-	},
-	"account12": {
-		"account": ["Account:", "Star Tribune"],
-		"email": ["Email:", "alex@gmail.com"],
-		"user": ["Username:", "Alex"],
-		"password": ["Password:", "alex123"],
-		"primary": ["Primary Account?:", "Yes"],
-		"type": ["Account Type:", "Other"],
-		"notes": ["Notes:", "For Star Tribune's online newspaper"]
-	},
-	"account13": {
-		"account": ["Account:", "Reddit"],
-		"email": ["Email:", "alex@gmail.com"],
-		"user": ["Username:", "AlexH"],
-		"password": ["Password:", "alexh123"],
-		"primary": ["Primary Account?:", "Yes"],
-		"type": ["Account Type:", "Other"],
-		"notes": ["Notes:", "Reddit account login"]
-	},
-	"account14": {
-		"account": ["Account:", "Guitar Center"],
-		"email": ["Email:", "alex@gmail.com"],
-		"user": ["Username:", "Alex123"],
-		"password": ["Password:", "alexh"],
-		"primary": ["Primary Account?:", "Yes"],
-		"type": ["Account Type:", "Other"],
-		"notes": ["Notes:", "GC.com shopping account"]
-	},
-	"account15": {
-		"account": ["Account:", "Lynda"],
-		"email": ["Email:", "alex@fullsail.edu"],
-		"user": ["Username:", "AlexH"],
-		"password": ["Password:", "alexh123"],
-		"primary": ["Primary Account?:", "Yes"],
-		"type": ["Account Type:", "School"],
-		"notes": ["Notes:", "Lynda.com school account"]
-	},
-	"account16": {
-		"account": ["Account:", "GAIN"],
-		"email": ["Email:", "alex@gmail.com"],
-		"user": ["Username:", "Alex"],
-		"password": ["Password:", "alex1"],
-		"primary": ["Primary Account?:", "Yes"],
-		"type": ["Account Type:", "Business"],
-		"notes": ["Notes:", "GAIN community account login"]
-	},
-	"account17": {
-		"account": ["Account:", "Adobe"],
-		"email": ["Email:", "alex@gmail.com"],
-		"user": ["Username:", "AlexHardtke"],
-		"password": ["Password:", "alexh111"],
-		"primary": ["Primary Account?:", "Yes"],
-		"type": ["Account Type:", "Other"],
-		"notes": ["Notes:", "Adobe user account"]
-	},
-	"account18": {
-		"account": ["Account:", "Avid"],
-		"email": ["Email:", "alex@gmail.com"],
-		"user": ["Username:", "Alex"],
-		"password": ["Password:", "alex123"],
-		"primary": ["Primary Account?:", "Yes"],
-		"type": ["Account Type:", "Personal"],
-		"notes": ["Notes:", "Avid account login"]
-	},
-	"account19": {
-		"account": ["Account:", "Netflix"],
-		"email": ["Email:", "hardtke@yahoo.com"],
-		"user": ["Username:", "Hardtke1"],
-		"password": ["Password:", "hardtke123"],
-		"primary": ["Primary Account?:", "Yes"],
-		"type": ["Account Type:", "Entertainment"],
-		"notes": ["Notes:", "Family's Netflix account"]
-	},
-	"account20": {
-		"account": ["Account:", "Yahoo Alternate"],
-		"email": ["Email:", "adam@yahoo.com"],
-		"user": ["Username:", "ADAM12"],
-		"password": ["Password:", "adam123"],
-		"primary": ["Primary Account?:", "No"],
-		"type": ["Account Type:", "Business"],
-		"notes": ["Notes:", "Adam's Yahoo account"]
+		"account": "Yahoo",
+		"email": "alex@yahoo.com",
+		"user": "alex@yahoo.com",
+		"password": "alex123",
+		"notes": "My Yahoo email account"
 	}
 }
--- ASD_Project/js/main.js
+++ ASD_Project/js/main.js
@@ -4,43 +4,76 @@
 
 $('#home').on('pageinit', function(){
 	//code for home page here
+
+	$("#jsonB").on('click', function(){
+		getJson();
+	});
+	$("#xmlB").on("click", function(){
+		getXml();
+	});	
+	$("#localB").on("click", function(){
+		getLocal();
+	});
+	
 	$("#cta p").css('text-align', 'center');
 	
 	$("h3").css('text-align', 'center');
 	
-	
-	
-	
 });
+
+
 
 //DOM Ready - Add Item
 
 
 $('#additem').on('pageinit', function(){
 
+
+
 // Variables
-	var storeData = function(data){
-		console.log(data);
-	};
+	
+	
+	var save = $('#submit');
 	
 	var autofillData = function (){
 		 
 	};
 	
-	var getData = function(){
-	
-	};
+/* var getData = function(){ }; */
 	
 // Click events for display, save and clear
 	var displayLink = $('#display');
 	displayLink.on("click", getData);
-	var save = $('#save');
-	save.on("click", validate);
+	var save = $('#submit');
+	save.on("click", myForm);
 	var clear = $("#clear");
 	clear.on("click", clearLocal);
-	
+
+// Clear Local
+	function clearLocal(){
+		if(localStorage.length === 0){
+			alert("There is no data to clear.")
+		}else{
+			localStorage.clear();
+			alert("All accounts are deleted!");
+			window.location.reload();
+			return false;
+		}
+	}
+
+// Delete Local
+	function deleteItem() {
+		if(confirm("Are you sure you want to delete this account?")){
+			localStorage.removeItem(this.key);
+			alert("Account was deleted!");
+			window.location.reload();
+		}else{
+			alert("Account was NOT deleted.");
+		}
+	}
+
 // Validate
-	var myForm = $('#addForm'),
+	var myForm            = $('#addForm'),
 		additemerrorslink = $('#additemerrorslink');
 		    
 	myForm.validate({
@@ -49,19 +82,17 @@ $('#additem').on('pageinit', function(){
 			var html = '';
 			for(var key in validator.submitted){
 				var label = $('label[for ^= "' + key +'"]');
-				console.log(label.text());
+				//console.log(label.text());
 				var legend = label.closest('fieldset').find('ui-controlgroup-label');
 				var fieldName = legend.length ? legend.text() : label.text();
-				html += '<li>'+ fieldName +'</li>';
-			};
-			$("#additemerrors ul").html(html);
-		},
+				html += '<li>'+ fieldName +'</li>'};
+			$("#additemerrors ul").html(html)},
 		submitHandler: function() {
-	var data = myForm.serializeArray();
-		storeData(data);
-		}
+		var data = myForm.serializeArray();
+		storeData(data);}
+		
 	});	
-	
+
 // Save
 	function storeData(key){
 	//If there is no key, this means this is a brand new item and we need a new key.
@@ -80,12 +111,14 @@ $('#additem').on('pageinit', function(){
 		item.user	  = ["Username:", $('#user').val()];
 		item.password = ["Password:", $('#password').val()];
 		item.notes    = ["Notes:", $('#notes').val()];
-	
+		
 	//Save data into LocalStorage.
 	localStorage.setItem(id, JSON.stringify(item));
 	alert("Account Saved!");
-	}
-	
+		console.log(key);
+
+	getData(key);
+	};
 	
 // Get Data
 	function getData(){
@@ -94,14 +127,14 @@ $('#additem').on('pageinit', function(){
 			alert("There is no data in Local Storage. Default data has been added.");
 			autoFillData();
 		}
-		
+		console.log(localStorage);
 		//Write data from Local Storage to the browser.
-		var makeDiv = $("<div></div>");
+		var makeDiv = $('<div></div>');
 		makeDiv.attr("id", "items");
-		var makeList = $("<ul></ul>");
+		var makeList = $('<ul></ul>');
 		makeDiv.append(makeList);
-		document.body.append(makeDiv);
-		$('items').style.display = "block";
+		$('body').append(makeDiv);
+		$('items').show();
 		for(var i=0,len=localStorage.length; i<len; i++){
 			var makeLi = $('<li></li>');
 			var linksLi = $('<li></li>');
@@ -109,18 +142,20 @@ $('#additem').on('pageinit', function(){
 			var key = localStorage.key(i);
 			var value = localStorage.getItem(key);
 			//Convert the string from local storage value back to an object by using JSON.parse().
-			var obj = JSON.parse(value);
+			var obj = jQuery.parseJSON(value);
 			var makeSubList = $('<ul></ul>');
-			makeLi.append(makeSubList);
-			getImage(obj.type[1], makeSubList);
-			for(var n in obj){
-				var makeSubLi = $('<li></li>');
+			makeLi.append(obj);
+			//getImage(obj.type[1], makeSubList);
+			/*
+for(var n in obj){
+				var makeSubLi = document.createElement('li');
 				makeSubList.append(makeSubLi);
 				var optSubText = obj[n][0]+" "+obj[n][1];
 				makeSubLi.innerHTML = optSubText;
 				makeSubList.append(linksLi);
 			}
-			//makeItemLinks(localStorage.key(i), linksLi);// Create our edit and delete buttons/links for each item in Local Storage.
+*/
+			makeItemLinks(localStorage.key(i), linksLi);// Create our edit and delete buttons/links for each item in Local Storage.
 		}
 	}
 	
@@ -128,17 +163,17 @@ $('#additem').on('pageinit', function(){
 	function editItem(){
 		//Grab the data from our item from Local Storage.
 		var value = localStorage.getItem(this.key);
-		var item = JSON.parse(value);
+		var item = jQuery.parseJSON(val());
 		
 		//Show the form
 		//toggleControls("off");
 		
 		//Populates the form fields with current localStorage values.
-		$('#account').value  = item.account[1];
-		$('#email').value    = item.email[1];
-		$('#user').value     = item.user[1];
-		$('#password').value = item.password[1];
-		$('#notes').value    = item.notes[1];
+		$('#account').val()  = item.account[1];
+		$('#email').val()    = item.email[1];
+		$('#user').val()     = item.user[1];
+		$('#password').val() = item.password[1];
+		$('#notes').val()    = item.notes[1];
 		
 		//Remove the initial listener for the input 'save account' button.
 		save.off("click", storeData);
@@ -152,6 +187,53 @@ $('#additem').on('pageinit', function(){
 
 		
 	}
+	
+//Get Image for the correct category that's being displayed
+	function getImage(catName, makeSubList){
+		var imageLi = $('<li></li>');
+		makeSubList.append(imageLi);
+		var newImg = $('<img></img>');
+		var setSrc = newImg.attr("src", "images/"+ catName + ".png");
+		imageLi.append(newImg);
+	}
+	
+	
+	
+//Auto Populate Local Storage
+	function autoFillData(){
+		//The actual JSON object data required for this to work comes from our json.js file which is loaded from our html page.
+		//Store the JSON object into Local Storage.
+		for(var n in json){
+			var id = Math.floor(Math.random()*100000001);
+			localStorage.setItem(id, JSON.stringify(json[n]));
+		}
+	}
+	
+//makeItemLinks function
+	function makeItemLinks(key, linksLi){
+		//Add edit item link
+		var editLink = $('<a></a>');
+		editLink.href = "#";
+		editLink.key = key;
+		var editText = "Edit Account";
+		editLink.on("click", editItem);
+		editLink.innerHTML = editText;
+		linksLi.append(editLink);
+		
+		//Add Line break
+		var breakTag = $('<br></br>');
+		linksLi.append(breakTag);
+		
+		//Add delete item link
+		var deleteLink = $('<a></a>');
+		deleteLink.href = "#";
+		deleteLink.key = key;
+		var deleteText = "Delete Account";
+		deleteLink.on("click", deleteItem);
+		deleteLink.innerHTML = deleteText;
+		linksLi.append(deleteLink);
+	}
+	
 });
 
 //DOM Ready - Show
@@ -159,6 +241,7 @@ $('#additem').on('pageinit', function(){
 
 $('#show').on('pageinit', function(){
 	//code for show page here
+	$("topnav").css("color", "red");
 	
 	$.ajax({
 		url: "xhr/list.php",
