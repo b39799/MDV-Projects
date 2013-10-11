1777440896042acd8532b7097df1f5b23f01e914
b39799 <hardtke.alex@fullsail.edu>
2013-10-10 21:04:23 -0500

    Added week 2

--- AVF/AVFproject/platforms/ios/www/js/index.js
+++ AVF/AVFproject/platforms/ios/www/js/index.js
@@ -46,6 +46,12 @@ var app = {
 
         console.log('Received Event: ' + id);
     }
+    
+    
+    
+    
+    
+    
     // var contacts = function(){};
     //Access contacts database.
     
@@ -61,3 +67,53 @@ var app = {
     // var storage = function(){};
     //Access to storage options.
 };
+
+$('#home').on('pageinit', function(){
+
+              
+              //Instagram
+              $(function(){
+                var tag= "cars";
+                var url= "https://api.instagram.com/v1/media/popular?client_id=56fd6edc02a84b3c8d4af1e3d7f3bbf8";
+                $.getJSON(url, screenOutput);
+  
+                });
+
+              var screenOutput = function(info){
+               // alert("screenOutput");
+                console.log(info);
+    
+              $("#instagram-msg").html("<h2>Instagram Results:</h2>")
+    
+              $.each(info.data, function(index, photo){
+                     var pic = "<li><img src='" + photo.images.standard_resolution.url + "' alt+'" + photo.user.id +
+                     "' /><h4>" + photo.user.full_name + ", <em>(" +photo.user.username +")</em></h4></li>";
+                     $("#instagram").append(pic);
+                });
+              };
+            
+
+});
+
+$('#home').on('pageinit', function(){
+
+              //ESPN
+              $(function(){
+                var url= "http://api.espn.com/v1/now/top?apikey=ds5dk4pnu2n3jaufgys7yynm";
+                $.getJSON(url, screenOutput);
+                });
+              
+              var screenOutput = function(info){
+              //alert("screenOutput");
+              console.log(info);
+              
+              $('#espn-msg').html("<h2>ESPN Top Headlines:</h2>")
+              
+              $.each(info.feed, function(index, item){
+                     var head = "<li><img src='" + item.images[0].url + "' /><h3>" + item.headline + "</h3></li>";
+                     $("#espn").append(head);
+                     
+                     });
+              }
+              
+});
